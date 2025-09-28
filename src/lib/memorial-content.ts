ad5897430f648f6effc1179e10006a8c5f34d27e
Renato Coutinho <futcout@gmail.com>
2025-09-28 05:40:50 +0000

    a primeira imagem dentro da aba do memorial ,substituia por essa imagem

--- src/lib/memorial-content.ts
+++ src/lib/memorial-content.ts
@@ -2,7 +2,7 @@
 import { PlaceHolderImages } from './placeholder-images';
 
 export const memorialPageContent = {
-  heroImageUrl: 'https://i.imgur.com/uG5zZ3H.jpeg',
+  heroImageUrl: 'https://prefeitura.rio/wp-content/uploads/2019/11/sumare-2019-foto-Angela-Meurer-scaled.jpg',
   heroTitle: 'Memorial Pet Estrela',
   heroDescription1: 'O Memorial Pet Estrela foi criado como uma forma carinhosa de eternizar a lembrança dos nossos animais que se tornaram estrelinhas. Aqui, cada vida é celebrada através do plantio de uma árvore, que simboliza amor, renovação e memória eterna.',
   heroDescription2: 'Além de homenagear nossos companheiros, este memorial também contribui para o reflorestamento, com mudas frutíferas e nativas, fortalecendo a natureza.',
